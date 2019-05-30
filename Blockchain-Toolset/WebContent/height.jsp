8913b89952b9c2be7f43c2169917fd432e5c2f53
Christopher <Cocopelly255@gmail.com>
2019-05-29 21:16:53 -0700

    Block by height is updating and displaying.
    Need to format text.

--- Blockchain-Toolset/WebContent/height.jsp
+++ Blockchain-Toolset/WebContent/height.jsp
@@ -1,20 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-      <link rel="stylesheet" href="styles.css">
-        <title>SO question 4112686</title>
-        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-        <script>
-            $(document).on("click", "#somebutton", function() {
-                $.get("BlockHeightServlet", function(responseText) {
-                    $("#somediv").text(responseText);
-                });
-            });
-        </script>
-    </head>
-    <body>
-        <p>Hello, World</p>
-        <button id="somebutton">press here</button>
-        <div id="somediv"></div>
-    </body>
+
+<head>
+
+
+
+
+
+
+  <link rel="stylesheet" href="styles.css">
+  <title>Block Height</title>
+  
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+
+  $(document).on("click", "#somebutton", function() {
+	    $.get("BlockHeightServlet", function(responseJson) {
+	        var $ul = $("<ul>").appendTo($("#somediv")); // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+	        $.each(responseJson, function(index, item) { // Iterate over the JSON array.
+	            $("<li>").text(item).appendTo($ul);      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+	        });
+	    });
+	    document.getElementById("somediv").innerHTML = "";
+	});
+  
+  
+  
+  
+  
+  </script>
+ 
+</head>
+
+<body>
+  <button id="somebutton">press here</button>
+  <div id="somediv"></div>
 </html>
--- Blockchain-Toolset/src/io/encryptech/BlockHeightServlet.java
+++ Blockchain-Toolset/src/io/encryptech/BlockHeightServlet.java
@@ -7,39 +7,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BlockHeightServlet
- */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 @WebServlet("/BlockHeightServlet")
 public class BlockHeightServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	final int SIZE = 33;
 
-    /**
-     * Default constructor. 
-     */
-    public BlockHeightServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-    @Override
+    @SuppressWarnings("resource")
+	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String text = "some text";
-
-        response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
-        response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+    	String out = "Failed to connect";
+    	try {
+        	out = new Scanner(new URL("https://blockchain.info/block-height/5?format=json").openStream(), "UTF-8").useDelimiter("\\A").next();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+		JSONArray jsonArray = null;
+		JSONObject jsnobject = new JSONObject(out);
+		jsonArray = jsnobject.getJSONArray("blocks");
+		String[] data = jsonArray.toString().split(",");
+    	
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+    	
+        List<String> list = new ArrayList<>();
+        String[] blockData = new String[SIZE];
         
-        response.getWriter().write(text);       // Write response body.
+        list.add(data[33]);		// height
+        list.add(data[32]);		// hash
+        list.add(data[1]);		// next block
+        list.add(data[2]);		// prev block
+        list.add(data[31]);		// time
+        list.add(data[21]);		// btc
+        
+        String json = new Gson().toJson(list);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    	
+    	/*
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(returnMe);
+        */
     }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
